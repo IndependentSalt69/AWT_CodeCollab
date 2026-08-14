bb815603138e8640baacc47cae5996fcb1178382
IndependentSalt69 <manavprasath03@gmail.com>
2026-08-14 11:31:35 +0530

    testing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,3 +3,9 @@ import React from 'react';
 export default function App() {
   return <div>CodeCollab Client</div>;
 }
+
+useEffect(() => {
+  fetch('http://localhost:5000/health')
+    .then(res => res.json())
+    .then(data => console.log(data));
+}, []);
--- server/src/index.js
+++ server/src/index.js
@@ -6,3 +6,6 @@ app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+const cors = require('cors');
+app.use(cors({ origin: 'http://localhost:3000' }));
